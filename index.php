8c42e6bc17f18d07640b95b9661daee7b20ab545
NabillaAuliaIF23B <if23.nabillaaulia@mhs.ubpkarawang.ac.id>
2024-11-21 07:24:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styleIndex.css">
+    <title>Ini Halaman Home</title>
+</head>
+<body>
+    <h1>Ini Halaman Login</h1>
+    <form action="./backend/login.php" method="POST" >
+        <input type="email" name="email" id="email" placeholder="Email">
+        <input type="password" name="password" id="password" placeholder="password">
+        <button type="submit">Login</button>
+    </form>
+
+    
+</body>
+</html>
